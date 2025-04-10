c21f5e76b2c2d58dfbe371e75bcddb3457a329b5
renzobaird <renzobaird@gmail.com>
2025-04-10 10:46:27 -0500

    more script editing

--- HEDGE_MAGE/Assets/Scripts/LetterManager.cs
+++ HEDGE_MAGE/Assets/Scripts/LetterManager.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class LetterManager
+public class LetterManager : MonoBehaviour
 {
-    public string[] wordList = {
+    public string[] wordList = { 
         "Doomcry",
         "Nighthex",
         "Soulrot",
@@ -29,11 +30,43 @@ public class LetterManager
         "Necrochant",
         "Blightward"
     };
+    public GameObject letterPrefab;
+    public List<Transform> spawnPoints;
+    public int numberOfDecoys = 5;
 
-    string selectedWord = wordList[Random.Range(0, wordList.Length)];
+    private string selectedWord;
 
-    char[] wordLetters = selectedWord.ToCharArray();
+    void Start()
+    {
+        selectedWord = wordList[Random.Range(0, wordList.Length)];
+        char[] letters = selectedWord.ToCharArray();
 
+        List<Transform> availableSpots = new List<Transform>(spawnPoints);
 
+        // Spawn correct letters
+        foreach (char letter in letters)
+        {
+            if (availableSpots.Count == 0) break;
 
+            int index = Random.Range(0, availableSpots.Count);
+            Transform spot = availableSpots[index];
+            availableSpots.RemoveAt(index);
+
+            GameObject obj = Instantiate(letterPrefab, spot.position, Quaternion.identity);
+            obj.GetComponent<LetterObject>().SetLetter(letter);
+        }
+
+        // Spawn decoys
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < numberOfDecoys && availableSpots.Count > 0; i++)
+        {
+            int index = Random.Range(0, availableSpots.Count);
+            Transform spot = availableSpots[index];
+            availableSpots.RemoveAt(index);
+
+            char randomLetter = alphabet[Random.Range(0, alphabet.Length)];
+            GameObject obj = Instantiate(letterPrefab, spot.position, Quaternion.identity);
+            obj.GetComponent<LetterObject>().SetLetter(randomLetter);
+        }
+    }
 }
